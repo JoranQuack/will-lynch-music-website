c6ad9f966ddd961159af68583b9dd00d619a576b
Joran <joran.lequellec@gmail.com>
2025-02-21 10:32:48 +1300

    fix: slide resisability

--- src/components/Homeslide.tsx
+++ src/components/Homeslide.tsx
@@ -64,11 +64,11 @@ export default function Homeslide() {
 
       <div
         id="slide1"
-        className={`absolute w-screen h-screen flex items-center justify-between pl-36 top-0 left-0 ease-in-out duration-500
+        className={`absolute w-screen h-screen flex items-center justify-between pl-36 top-0 left-0 ease-in-out duration-500 gap-80
                 ${slide === 1 ? "opacity-100 translate-x-0 z-10" : ""}
-                ${slide === 2 ? "opacity-100 -translate-x-[150%] z-0" : ""}
-                ${slide === 3 ? "opacity-0 -translate-x-[200%] z-0" : ""}
-                ${slide === 4 ? "opacity-0 -translate-x-[200%] z-0" : ""}`}
+                ${slide === 2 ? "opacity-100 -translate-x-[2300px] z-0" : ""}
+                ${slide === 3 ? "opacity-0 -translate-x-[4000px] z-0" : ""}
+                ${slide === 4 ? "opacity-0 -translate-x-[4000px] z-0" : ""}`}
       >
         <div className="w-2/5 flex flex-col gap-5">
           <p className="text-6xl duration-500" style={{ color: textColour }}>
@@ -90,7 +90,7 @@ export default function Homeslide() {
         </div>
         <SVG
           src="stave.svg"
-          className="ml-72 duration-500 overflow-visible"
+          className="duration-500 overflow-visible w-full h-full scale-90 translate-y-[10vh]"
           style={{ fill: textColour }}
         />
       </div>
@@ -126,10 +126,10 @@ export default function Homeslide() {
 
       <div
         id="slide3"
-        className={`absolute w-screen h-screen flex items-center justify-between pl-36 top-0 left-0 ease-in-out duration-500
+        className={`absolute w-screen h-screen flex items-center justify-between pl-36 top-0 left-0 ease-in-out duration-500 gap-20
                 ${slide === 3 ? "opacity-100 z-10" : "opacity-0 z-0"}`}
       >
-        <div className="w-4/5 flex flex-col gap-5">
+        <div className="w-3/5 flex flex-col gap-5">
           <p className="text-6xl duration-500" style={{ color: textColour }}>
             <b>DEVELOP</b> YOUR <br />
             VOCAL <b>POTENTIAL</b>
@@ -148,7 +148,7 @@ export default function Homeslide() {
         </div>
         <SVG
           src="mic.svg"
-          className="ml-20 duration-500 scale-[400%] -translate-x-80 translate-y-[120%]"
+          className="duration-500 h-full w-full translate-y-1/2 scale-150"
           style={{ fill: textColour }}
         />
       </div>
