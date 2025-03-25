7f900a22e53d6ba430b56699fe3dba807ed4d666
Joran <joran.lequellec@gmail.com>
2025-03-25 23:25:55 +1300

    feat: mobile friendly footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import SVG from "react-inlinesvg";
 
 export default function Footer() {
   return (
-    <footer className="relative flex items-end justify-between overflow-hidden bg-green px-36 pb-20 pt-72">
+    <footer className="relative flex flex-col items-center justify-between gap-2 overflow-hidden bg-green px-8 pb-20 pt-72 lg:flex-row lg:items-end lg:gap-0 lg:px-36">
       <Link href="/">
         <SVG src="/logo.svg" className="fill-white" />
       </Link>
-      <div className="flex flex-col items-end gap-2 text-right">
+      <div className="flex flex-col items-center gap-2 text-center lg:items-end lg:text-right">
         <p className="text-white">Site by Joran Le Quellec</p>
         <p className="text-white">© {new Date().getFullYear()} Will Lynch</p>
       </div>
