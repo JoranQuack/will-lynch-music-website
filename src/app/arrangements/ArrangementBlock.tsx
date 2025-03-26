ba20ccf54a59310c36d698c9b07df052e50b3e6c
Joran <joran.lequellec@gmail.com>
2025-03-26 15:49:00 +1300

    feat: mobile arrangement blocks

--- src/app/arrangements/ArrangementBlock.tsx
+++ src/app/arrangements/ArrangementBlock.tsx
@@ -53,7 +53,7 @@ export default function ArrangementBlock({
           alt={src}
           className="rounded-xl"
         />
-        <p>{title}</p>
+        <p className="break-words">{title}</p>
       </div>
       {isOpen && (
         <motion.div
@@ -61,11 +61,11 @@ export default function ArrangementBlock({
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="fixed inset-0 z-40 flex cursor-pointer items-center justify-center backdrop-blur-lg"
+          className="fixed inset-0 z-40 flex cursor-pointer items-center justify-center p-8 backdrop-blur-lg lg:p-0"
           onClick={() => setIsOpen(false)}
         >
           <div
-            className="cursor-default rounded-xl bg-white p-3"
+            className="w-full cursor-default rounded-xl bg-white p-3 lg:w-fit lg:max-w-screen-lg"
             onClick={handleChildClick}
           >
             <div className="flex w-full justify-end">
@@ -76,7 +76,7 @@ export default function ArrangementBlock({
                 <IconX className="h-full" />
               </button>
             </div>
-            <div className="flex gap-10 px-5 pb-5">
+            <div className="flex flex-col items-center gap-2 px-5 pb-5 lg:flex-row lg:items-stretch lg:gap-10">
               <Image
                 src={src}
                 height={400}
@@ -84,22 +84,24 @@ export default function ArrangementBlock({
                 alt={src}
                 className="rounded-xl"
               />
-              <div className="flex flex-col justify-between py-5">
+              <div className="flex flex-col justify-between py-2 lg:py-5">
                 <div className="flex flex-col gap-2">
-                  <h2 className="text-3xl text-green">{title}</h2>
+                  <h2 className="text-2xl text-green lg:text-3xl">{title}</h2>
                   {inspiredBy === "---" ? (
                     ""
                   ) : (
-                    <sub className="-mt-2 text-lg text-green">
+                    <sub className="-mt-2 text-base text-green">
                       Inspired by {inspiredBy}
                     </sub>
                   )}
                   {arrangedFor === "---" ? (
                     ""
                   ) : (
-                    <p>Arranged for {arrangedFor}</p>
+                    <p className="text-sm lg:text-base">
+                      Arranged for {arrangedFor}
+                    </p>
                   )}
-                  <div className="mt-3 flex gap-2 [&>p]:rounded-full [&>p]:p-1 [&>p]:px-3">
+                  <div className="mt-3 flex flex-wrap gap-2 text-sm lg:text-base [&>p]:h-fit [&>p]:rounded-full [&>p]:p-1 [&>p]:px-3">
                     <p className="bg-bluey">{voicings}</p>
                     <p className="bg-pinky">{parts} Parts</p>
                     <p className="bg-greeny">{purpose}</p>
@@ -107,50 +109,50 @@ export default function ArrangementBlock({
                       {difficulty[0].toUpperCase() + difficulty.substring(1)}{" "}
                       Difficulty
                     </p>
-                  </div>
-                  <div className="flex gap-2 [&>p]:rounded-full [&>p]:p-1 [&>p]:px-3">
                     <p className="bg-greeny">
                       {tempo[0].toUpperCase() + tempo.substring(1)} Tempo
                     </p>
                     <p className="bg-bluey">{style.replaceAll("/ ", "/")}</p>
                   </div>
                 </div>
-                {smp === "Contact Me!" ? (
-                  <div className="flex flex-col gap-2">
-                    <p>Available for purchase:</p>
-                    <a
-                      href="#contact"
-                      onClick={() => setIsOpen(false)}
-                      className="round-button bg-green text-white"
-                    >
-                      GET IN TOUCH
-                    </a>
-                  </div>
-                ) : (
-                  <div className="flex flex-col gap-2">
-                    <p>Buy online:</p>
-                    <div className="flex gap-2">
-                      <a href={smd} target="_blank" rel="noreferrer">
-                        <Image
-                          src="/sheetmusicdirect.png"
-                          className="size-16 rounded-xl"
-                          width={100}
-                          height={100}
-                          alt="Sheet Music Direct"
-                        />
-                      </a>
-                      <a href={smp} target="_blank" rel="noreferrer">
-                        <Image
-                          src="/sheetmusicplus.png"
-                          className="size-16 rounded-xl"
-                          width={100}
-                          height={100}
-                          alt="Sheet Music Direct"
-                        />
+                <div className="mt-5 flex flex-col gap-2">
+                  {smp === "Contact Me!" ? (
+                    <div>
+                      <p className="mb-5">Available for purchase:</p>
+                      <a
+                        href="#contact"
+                        onClick={() => setIsOpen(false)}
+                        className="round-button bg-green text-white"
+                      >
+                        GET IN TOUCH
                       </a>
                     </div>
-                  </div>
-                )}
+                  ) : (
+                    <div>
+                      <p>Buy online:</p>
+                      <div className="flex gap-2">
+                        <a href={smd} target="_blank" rel="noreferrer">
+                          <Image
+                            src="/sheetmusicdirect.png"
+                            className="size-16 rounded-xl"
+                            width={100}
+                            height={100}
+                            alt="Sheet Music Direct"
+                          />
+                        </a>
+                        <a href={smp} target="_blank" rel="noreferrer">
+                          <Image
+                            src="/sheetmusicplus.png"
+                            className="size-16 rounded-xl"
+                            width={100}
+                            height={100}
+                            alt="Sheet Music Direct"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
