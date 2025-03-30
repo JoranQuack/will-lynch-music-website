e9c4aef6a65e347d52427d28c4a6291d9da56d0e
Joran <joran.lequellec@gmail.com>
2025-03-30 14:11:29 +1300

    feat: SEO robots and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,9 +3,9 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: "*", // Apply to all bots
-      disallow: "/", // Disallow everything
+      userAgent: "*",
+      allow: "/",
     },
-    // sitemap: 'https://www.willlynchmusic.co.nz/sitemap.xml',
+    sitemap: "https://www.willmclynchmusic.com/sitemap.xml",
   };
 }
