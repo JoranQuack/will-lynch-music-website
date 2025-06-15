7bf2e75b986b8da5adf617efd3f0a9995e28738c
Joran <joran.lequellec@gmail.com>
2025-06-15 12:48:59 +1200

    update domain name

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.willmclynchmusic.com",
+      url: "https://www.willlynchmusic.com",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.willmclynchmusic.com/arrangements",
+      url: "https://www.willlynchmusic.com/arrangements",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://www.willmclynchmusic.com/tracks",
+      url: "https://www.willlynchmusic.com/tracks",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
